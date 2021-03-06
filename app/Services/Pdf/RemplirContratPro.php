c2251a58610a20ee3d6a76fc8b2b08cc0fdad2cb
Roxel Lay <contact@romukulot.fr>
2021-03-06 17:14:38 +0100

    Méthodes print() dans RemplirContratPro

--- app/Services/Pdf/RemplirContratPro.php
+++ app/Services/Pdf/RemplirContratPro.php
@@ -52,4 +52,54 @@ class RemplirContratPro implements PdfService
         $this->pdf->cell(101, 5,
             utf8_decode($this->model->employeur->denomination));
     }
+
+    private function printNoAdresse()
+    {
+        $this->pdf->setXY(17, 77.5);
+        $this->pdf->cell(50, 5,
+            utf8_decode($this->model->employeur->noAdresse));
+    }
+
+    private function printVoieAdresse()
+    {
+        $this->pdf->setXY(54, 77.5);
+        $this->pdf->cell(70, 5,
+            utf8_decode($this->model->employeur->voieAdresse));
+    }
+
+    private function printComplementAdresse()
+    {
+        $this->pdf->setXY(34, 84);
+        $this->pdf->cell(70, 5,
+            utf8_decode($this->model->employeur->complementAdresse));
+    }
+
+    private function printCodePostalEmployeur()
+    {
+        $this->pdf->setXY(33, 90);
+        $this->pdf->cell(70, 5,
+            utf8_decode($this->model->employeur->codePostal));
+    }
+
+    private function printCommuneEmployeur()
+    {
+        $this->pdf->setXY(31, 96.5);
+        $this->pdf->cell(70, 5,
+            utf8_decode($this->model->employeur->commune));
+    }
+
+    private function printTelephoneEmployeur()
+    {
+        $x = 25;
+        $y = 103.5;
+        $telephone = str_split($this->model->employeur->telephone);
+        // str_split() enlève le '0' initial du numéro...
+        array_unshift($telephone, '0');
+        foreach($telephone as $i => $chiffre) {
+            $this->pdf->cell(3, 5, $chiffre);
+            if($i % 2 == 0) $x += 5.3;
+            else            $x += 4;
+            $this->pdf->setXY($x, $y);
+        }
+    }
 }
