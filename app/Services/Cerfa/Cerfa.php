2b0228edfd7ffe4d797e57574fe125f550f626cb
Roxel Lay <contact@romukulot.fr>
2021-03-08 09:48:03 +0100

    Implémentation de méthodes

--- app/Services/Cerfa/Cerfa.php
+++ app/Services/Cerfa/Cerfa.php
@@ -6,14 +6,17 @@ use Illuminate\Http\Response;
 
 class Cerfa
 {
-    private ?string $id = '';
-    private array $pages = [];
-    private ?CerfaConfig $cerfaConfig = null;
+    private ?string $id;
+    private array $pages;
+    private ?CerfaConfig $cerfaConfig;
     private int $pageCount = 1;
 
     public function __construct(CerfaConfig $cerfaConfig)
     {
+        $this->id =
         $this->cerfaConfig = $cerfaConfig;
+        $this->pages = [];
+
         $this->initialize();
     }
 
@@ -22,11 +25,14 @@ class Cerfa
         $config = $this->cerfaConfig->getConfig();
 
         $this->id = $config->cerfa;
-        foreach($config->pages as $configPage) {
-            $page = new Page();
+        foreach($config->pages as $noPage => $configPage) {
+            $page = new Page($noPage);
             foreach($configPage->fields as $configField) {
                 $field = new Field($configField);
+                $page->addField($field);
             }
+            $this->addPage($page);
+            $this->pageCount++;
         }
     }
 
--- app/Services/Cerfa/Text.php
+++ app/Services/Cerfa/Text.php
@@ -4,5 +4,11 @@ namespace App\Services\Cerfa;
 
 class Text extends Field
 {
+    public ?float $spacing;
 
+    public function __construct(\stdClass $config)
+    {
+        parent::__construct($config);
+        $this->spacing = $config->spacing;
+    }
 }
